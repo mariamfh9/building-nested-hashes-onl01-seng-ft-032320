dd6465d4ec78fa9001e538102bb2a8df2f9264ec
Mariam Haider <mfh.haider@gmail.com>
2020-03-27 16:34:55 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -6,7 +6,7 @@ puts epic_tragedy
 #  =>
 {
    :montague => {
-      :patriarch => {name: "Michael Jordan", age: "53"},
+      :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
